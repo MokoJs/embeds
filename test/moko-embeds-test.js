b5671e85aa76d645f8c8bc3d7fe2593f10fefde9
Ryan Schmukler <rschmukler@gmail.com>
2014-06-21 17:31:57 -0400

    in-progress commit

---
+++ test/moko-embeds-test.js
@@ -0,0 +1,27 @@
+var expect = require('expect.js'),
+    moko   = require('moko'),
+    embeds = require('../');
+
+
+describe('moko-embeds', function() {
+  var Color = moko('Color').attr('name');
+  var Account = moko('Account').attr('balance').attr('type');
+
+  var User = moko('User')
+    .attr('username')
+    .attr('favoriteColor', { embeds: Color })
+    .attr('accounts', { embeds: Account });
+
+  before(function*() {
+    User = moko('User');
+  });
+
+  it('is a moko plugin', function() {
+    expect(embeds).to.be.a(Function);
+  });
+
+  it('initializes embeddable single-documents', function*() {
+    var user = yield new User({username: 'Bob', favoriteColor: { name: 'Blue' }});
+    expect(user.favoriteColor).to.be.a(Color);
+  });
+});
